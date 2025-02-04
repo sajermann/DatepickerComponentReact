78f327cafbc679a112464d5a21b0785f7fa4bbad
Sajermann <sajermannbruno@gmail.com>
2025-02-04 19:13:57 -0300

    fix

--- src/Components/RoutesConfig/Sidebar/Config/index.test.tsx
+++ src/Components/RoutesConfig/Sidebar/Config/index.test.tsx
@@ -4,16 +4,16 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { InjectorProviders } from '~/components/InjectorProviders';
-import { Credits } from '.';
+import { Config } from '.';
 
 describe('Components/shared/RoutesConfig/Sidebar/Credits', () => {
   it(`should render component`, async () => {
     const { queryByText } = render(
       <InjectorProviders>
-        <Credits />
+        <Config />
       </InjectorProviders>,
     );
 
-    expect(queryByText(/Bruno Sajermann/g)).toBeTruthy();
+    expect(queryByText(/Settings/g)).toBeTruthy();
   });
 });
