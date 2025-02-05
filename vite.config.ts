d148869a9466bd49315ba8a264d7eabe73baf887
Sajermann <sajermannbruno@gmail.com>
2025-02-05 11:44:47 -0300

    add vite plugin pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import Pages from 'vite-plugin-pages';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -15,7 +16,7 @@ export default defineConfig(({ mode }) => {
     preview: {
       port: 8080,
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), Pages()],
     resolve: {
       alias: {
         '~': path.resolve(__dirname, 'src'),
