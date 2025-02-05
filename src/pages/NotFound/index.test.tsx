63a17eadd4da0c3319c008dacd296c0f064e5171
Sajermann <sajermannbruno@gmail.com>
2025-02-05 15:08:29 -0300

    fix

--- src/pages/NotFound/index.test.tsx
+++ src/pages/NotFound/index.test.tsx
@@ -2,18 +2,18 @@
  * @vitest-environment jsdom
  */
 import { render } from '@testing-library/react';
-import { it, describe } from 'vitest';
-import { InjectorProviders } from '~/Components/InjectorProviders';
+import { describe, expect, it } from 'vitest';
+import { InjectorProviders } from '~/components/InjectorProviders';
 import { NotFoundPage } from '.';
 
 describe('Pages/NotFoundPage', () => {
-	it(`should render component`, async () => {
-		const { getByText } = render(
-			<InjectorProviders>
-				<NotFoundPage />
-			</InjectorProviders>,
-		);
+  it(`should render component`, async () => {
+    const { getByText } = render(
+      <InjectorProviders>
+        <NotFoundPage />
+      </InjectorProviders>,
+    );
 
-		expect(getByText('Not Found Page')).toBeTruthy();
-	});
+    expect(getByText('Not Found Page')).toBeTruthy();
+  });
 });
